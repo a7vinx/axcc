d62610b1a1b5844e707d729740cc1b49d05a32d7
Arvin Hsu <arvin.x@icloud.com>
2018-03-10 22:22:09 +0800

    Add classes of Token and TokenSequence

---
+++ src/token.cc
@@ -0,0 +1,69 @@
+#include "token.hh"
+
+namespace axcc {
+
+std::string LocStr(const SourceLocation& loc) {
+    std::string locstr;
+    locstr += *loc.fnamep;
+    locstr += ":";
+    locstr += std::to_string(loc.row);
+    locstr += ":";
+    locstr += std::to_string(loc.column);
+    locstr += ": ";
+    int loc_prefix_len = locstr.size();
+    std::string linestr{loc.linep, loc.linep + loc.line_len};
+    linestr += '\n';
+    // Format by trimming
+    int skipn = linestr.find_first_not_of(" \t\n\r\f\v");
+    linestr.erase(0, skipn);
+    locstr += linestr;
+    // Add '^' in the next line to point to target token
+    locstr += std::string(loc_prefix_len + loc.column - skipn - 1, ' ');
+    locstr += '^';
+    locstr += '\n';
+    return locstr;
+}
+
+TokenSequence::TokenSequence(const TokenSequence& other) {
+    for (auto const &tp : other.token_list_) {
+        token_list_.push_back(std::make_unique<Token>(*tp));
+    }
+}
+
+Token* TokenSequence::Begin() {
+    token_list_iter_ = token_list_.begin();
+    return Next();
+}
+
+Token* TokenSequence::Next() {
+    if (token_list_iter_ == token_list_.end())
+        return nullptr;
+    auto ret = token_list_iter_;
+    std::advance(token_list_iter_, 1);
+    return &(**ret);
+}
+
+Token* TokenSequence::LookAheadN(int n) const {
+    if (std::distance({token_list_iter_}, token_list_.cend()) < n)
+        return nullptr;
+    return &(**std::next(token_list_iter_, n - 1));
+}
+
+void TokenSequence::ReplacePrevN(int n, TokenSequence&& ts) {
+    ErasePrevN(n);
+    std::move(ts.token_list_.begin(), ts.token_list_.end(),
+              std::inserter(token_list_, token_list_iter_));
+    // Set the iterator pointing to the first token in the inserted token list.
+    std::advance(token_list_iter_, -ts.token_list_.size());
+}
+
+void TokenSequence::ReplacePrevN(int n, const std::vector<Token>& tv) {
+    ErasePrevN(n);
+    for (auto const& t : tv) {
+        token_list_.insert(token_list_iter_, std::make_unique<Token>(t));
+    }
+    std::advance(token_list_iter_, -tv.size());
+}
+
+}
+
---
+++ src/token.hh
@@ -0,0 +1,162 @@
+#ifndef _AXCC_TOKEN_HH_
+#define _AXCC_TOKEN_HH_
+
+#include <string>
+#include <list>
+#include <vector>
+#include <iterator>
+#include <memory>
+#include <cstdlib>
+#include <utility>
+
+namespace axcc {
+
+struct SourceLocation {
+    const std::string* fnamep;
+    unsigned int row;
+    unsigned int column;
+    std::string::const_iterator linep;
+    std::size_t line_len;
+};
+
+// Return string like "token.hh:19:36: source code"
+std::string LocStr(const SourceLocation& loc);
+
+enum class TokenType {
+    // Punctuators
+    DOT, COMMA, COLON, SEMI,
+    EXCL, QUES, AST, PCT,
+    AMP, VBAR, TILDE, CARET,
+    ASGN, PLUS, MINUS,
+    SLASH, BSLASH,
+    LSQUO, RSQUO,
+    LDQUO, RDQUO,
+    LPAR, RPAR,
+    LBRACE, RBRACE,
+    LBRACKET, RBRACKET,
+    LSAQUO, RSAQUO,
+    // multi-punctuator operators
+    LOGICAL_AND, LOGICAL_OR,
+    SHL, SHR, INC, DEC, EQ, NEQ,
+    ADD_ASGN, SUB_ASGN, MUL_ASGN,
+    DIV_ASGN, AND_ASGN, OR_ASGN,
+    XOR_ASGN, SHL_ASGN, SHR_ASGN,
+    MOD_ASGN, ARROW, ELLIP,
+    // Type specifiers
+    VOID, CHAR, SHORT,
+    INT, LONG, LLONG,
+    FLOAT, DOUBLE, BOOL,
+    UNSIGNED, SIGNED,
+    STRUCT, UNION, ENUM,
+    // Type qualifiers
+    // _Atomic can also be type specifier
+    CONST, VILATILE, RESTRICT,
+    ATOMIC, COMPLEX, IMAGINARY,
+    // Storage-class specifiers
+    STATIC, EXTERN, AUTO, REIGSTER, THREAD_LOCAL,
+    // Function specifiers
+    INLINE, NO_RETURN,
+    // Other keywords
+    IF, ELSE, FOR, WHILE, DO, BREAK,
+    SWITCH, CASE, GOTO, CONTINUE,
+    TYPEDEF, RETURN, FUNC_NAME,
+    STATIC_ASSERT, GENERIC,
+    SIZEOF, ALIGNAS, ALIGNOF,
+    // Preprocessor directives
+    PP_IF, PP_ELSE, PP_ELIF, PP_ENDIF,
+    PP_DEF, PP_IFDEF, PP_IFNDEF, PP_UNDEF,
+    PP_INCLUDE,
+    PP_LINE,
+    PP_ERROR,
+    // Users' tokens
+    IDENTIFIER, COMMENT, STRING,
+    I_CONSTANT, F_CONSTANT,
+    // Others
+    NEW_LINE
+};
+
+class Token {
+public:
+    Token(const TokenType& tag, const std::string& token_str,
+          const std::shared_ptr<const SourceLocation>& locp)
+        : tag_{tag}, token_str_{token_str}, locp_{locp} {}
+    // No loction pointer version for constructing the token whose location
+    // pointer is meaningless.
+    Token(const TokenType& tag, const std::string& token_str)
+        : tag_{tag}, token_str_{token_str} {}
+    Token(const Token&) = default;
+    Token(Token&&) = default;
+    Token& operator=(const Token&) = delete;
+    Token& operator=(Token&&) = delete;
+    virtual ~Token() = default;
+
+    TokenType Tag() const { return tag_; }
+    std::string TokenStr() const { return token_str_; }
+    std::shared_ptr<const SourceLocation> LocPtr() const { return locp_; }
+    void SetLocPtr(const std::shared_ptr<const SourceLocation>& locp) {
+        locp_ = locp;
+    }
+
+private:
+    const TokenType tag_;
+    // Integer and float constant is stored as string for now.
+    const std::string token_str_;
+    std::shared_ptr<const SourceLocation> locp_{};
+};
+
+inline bool operator==(const Token& lhs, const Token& rhs) {
+    return (lhs.Tag() == rhs.Tag()) && (lhs.TokenStr() == rhs.TokenStr());
+}
+inline bool operator!=(const Token& lhs, const Token& rhs) {
+    return !(lhs == rhs);
+}
+
+class TokenSequence {
+public:
+    TokenSequence() = default;
+    // Deep copy
+    TokenSequence(const TokenSequence& other);
+    TokenSequence(TokenSequence&&) = default;
+    TokenSequence& operator=(const TokenSequence&) = delete;
+    TokenSequence& operator=(TokenSequence&&) = delete;
+    virtual ~TokenSequence() = default;
+
+    // Simple wrapper functions for adding tokens from scratch one by one.
+    template<typename... Args>
+    void EmplaceBack(Args&&... args) {
+        token_list_.emplace_back(
+                        std::make_unique<Token>(std::forward<Args>(args)...));
+    }
+    void PushBack(const Token& t) {
+        token_list_.push_back(std::make_unique<Token>(t));
+    }
+
+    // Helper functions for iterating token sequence.
+    // All the following functions can only be used after Begin() has been
+    // called and the parameter n, if it has, should be positive (no check
+    // is performed inside).
+    // (Maybe we can add a Token::END type so that we can return reference
+    // type here?)
+    Token* Begin();
+    Token* Next();
+    Token* LookAhead() const { return LookAheadN(1); }
+    Token* LookAheadN(int n) const;
+    // Wrapper functions for adjusting the token list.
+    // The previous n tokens indicated by the parameter n include the current
+    // token (i.e., if n = 1 only the current token will be erased).
+    void ErasePrevN(int n) {
+        token_list_.erase(std::prev(token_list_iter_, n), token_list_iter_);
+    }
+    void ReplacePrevN(int n, TokenSequence&& ts);
+    void ReplacePrevN(int n, const std::vector<Token>& tv);
+
+private:
+    // Use pointers here to speed up the merging process of two token
+    // sequence (Is this really useful?).
+    std::list<std::unique_ptr<Token>> token_list_{};
+    // This iterator points to the next token in the list, not the current one.
+    std::list<std::unique_ptr<Token>>::iterator token_list_iter_{};
+};
+
+}
+#endif
